cbba2471a0d89b3f1534a91276387bb1a9437e7e
Davy Beauzil <davy.beauzil72@gmail.com>
2022-01-18 10:03:30 +0100

    installation of ecs, phpstan, rector, phpunit & composer-unused

---
+++ tests/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Tests;
+
+include_once __DIR__.'/../vendor/autoload.php';
+
+$classLoader = new \Composer\Autoload\ClassLoader();
+$classLoader->addPsr4("App\\Tests\\", __DIR__, true);
+$classLoader->register();
